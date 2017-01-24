b334c6edfdac8a67251b12b1fe225dec4ee6970d
c00z <zcusimano@gmail.com>
2017-01-24 15:57:00 -0800

    caretakers and healthy tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124232348) do
+ActiveRecord::Schema.define(version: 20170124235456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "caretakers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "department"
+    t.string   "access"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "fish", force: :cascade do |t|
     t.string   "species",                        null: false
@@ -21,6 +29,8 @@ ActiveRecord::Schema.define(version: 20170124232348) do
     t.integer  "age",        default: -1
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+    t.string   "tag"
+    t.boolean  "healthy",    default: true
   end
 
 end
